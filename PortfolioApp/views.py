88508f685f4029a84e7999683537dc41f31ae995
arnavsaha26 <arnav@rhythmflows.com>
2024-11-25 19:16:40 +0530

    Committed changes

--- PortfolioApp/views.py
+++ PortfolioApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from requests import Response
 
 # Create your views here.
+
+class ExampleAPIView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello from Django!"})
+
+    def post(self, request):
+        data = request.data  # Data sent from the frontend
+        return Response({"received": data})
